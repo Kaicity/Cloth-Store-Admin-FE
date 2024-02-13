951c04fb78ecbc7d91f87977663010bb7b7d93b6
Nguyễn Minh Thông <nguyenminhthongitmix@gmail.com>
2024-02-13 16:09:23 +0000

    Complete

--- src/app/core/apis/Dtos/ExportingBillFullDto.ts
+++
@@ -1,7 +0,0 @@
-import {ExportingBillDto} from "./ExportingBillDto";
-import {ExportingBillTransactionDto} from "./ExportingBillTransactionDto";
-
-export class ExportingBillFullDto {
-  exportingBill!: ExportingBillDto | null;
-  exportingBillTransactions!: ExportingBillTransactionDto[] | null;
-}
--- src/app/core/apis/Dtos/SizesDto.ts
+++
@@ -1,9 +0,0 @@
-import {ProductDto} from "./ProductDto";
-import {OptionDto} from "./OptionDto";
-
-export class SizesDto {
-  id!: string | null;
-  optionProductDto!: OptionDto | null;
-  addition!: number | null;
-  productDto!: ProductDto | null;
-}
